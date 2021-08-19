2934cbdcedf0c5705c8fabcd62da19b91c2135b3
rawin-chandra <40013619+rawin-chandra@users.noreply.github.com>
2021-08-19 15:54:47 +0700

    Add files via upload

---
+++ matplot_excel_factory.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import csv
+
+shake_force = []
+shake_freq = []
+fail_prob = []
+'''
+with open('/home/rawin/data_set2.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    plots.next()
+    for row in plots:
+        shake_force.append(int(row[2]))
+        shake_freq.append(int(row[3]))
+        fail_prob.append(float(row[4]))
+'''
+'''
+c = 0
+with open('/home/rawin/data_set2.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    plots.next()
+    for row in plots:
+        shake_force.append(int(row[2]))
+        shake_freq.append(int(row[3]))
+        fail_prob.append(float(row[4]))
+        if c > 100:
+            break
+        c += 1
+'''
+
+'''
+with open('/home/rawin/data_set2.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    plots.next()
+    for row in plots:
+        if float(row[4]) > 0.7:
+            shake_force.append(int(row[2]))
+            shake_freq.append(int(row[3]))
+            fail_prob.append(float(row[4]))
+'''
+
+with open('/home/rawin/data_set2.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    plots.next()
+    for row in plots:
+        if float(row[4]) > 0.7:
+            #shake_force.append(int(row[2]))
+            shake_freq.append(int(row[3]))
+            fail_prob.append(float(row[4]))
+
+#plt.plot(shake_force, marker = 'o')
+plt.plot(shake_freq, marker = '*')
+plt.plot(fail_prob, marker = 'x')
+plt.show()
